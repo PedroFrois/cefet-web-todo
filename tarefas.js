4d4cc279cdf0eafa870129778db22023758f8868
Pedro Bittencourt <p.f.bit@hotmail.com>
2021-07-16 23:19:39 -0300

    feat: Exercício 1

--- tarefas.js
+++ tarefas.js
@@ -4,9 +4,27 @@ class Tarefa {
         this.categoria = categoria;
         this.realizada = realizada;
     }
+
+    adicionaNaPagina(containerEl) {
+        let novoItemLi = document.createElement('li')
+        novoItemLi.classList.toggle('item-tarefa')
+        if (this.realizada) {
+            novoItemLi.classList.toggle('marcado')
+        }
+        novoItemLi.classList.toggle(`categoria-${this.categoria}`)
+        novoItemLi.innerHTML = this.nome
+        containerEl.appendChild(novoItemLi)
+    }
 }
 
 let tarefas = []
 
 tarefas.push(new Tarefa('Comprar leite', 'compras', false))
 tarefas.push(new Tarefa('Escutar chimbinha', 'lazer', true))
+
+let listaTarefas = document.querySelectorAll('#lista-tarefas')[0]
+listaTarefas.innerHTML = ""
+
+tarefas.forEach(tarefa => {
+    tarefa.adicionaNaPagina(listaTarefas)
+})
