aff5bc8b920b59d50c855c5360e67dd6080bd15b
Pedro Bittencourt <p.f.bit@hotmail.com>
2021-07-16 23:41:34 -0300

    feat: Exercício 2

--- tarefas.js
+++ tarefas.js
@@ -22,9 +22,22 @@ let tarefas = []
 tarefas.push(new Tarefa('Comprar leite', 'compras', false))
 tarefas.push(new Tarefa('Escutar chimbinha', 'lazer', true))
 
-let listaTarefas = document.querySelectorAll('#lista-tarefas')[0]
-listaTarefas.innerHTML = ""
+let itemDOMCampoNovaTarefaNome = document.querySelectorAll('#nova-tarefa-nome')[0]
+let itemDOMCampoNovaTarefaCategoria = document.querySelectorAll('#nova-tarefa-categoria')[0]
+let itemDOMBotaoAdicionar = document.querySelectorAll('#incluir-nova-tarefa')[0]
+let itemDOMListaTarefas = document.querySelectorAll('#lista-tarefas')[0]
+itemDOMListaTarefas.innerHTML = ""
 
 tarefas.forEach(tarefa => {
-    tarefa.adicionaNaPagina(listaTarefas)
+    tarefa.adicionaNaPagina(itemDOMListaTarefas)
 })
+
+itemDOMBotaoAdicionar.addEventListener('click', e => {
+    let novaTarefa = new Tarefa(itemDOMCampoNovaTarefaNome.value, itemDOMCampoNovaTarefaCategoria.value, false)
+    if (novaTarefa.nome.trim() != '') {
+        tarefas.push(novaTarefa)
+        novaTarefa.adicionaNaPagina(itemDOMListaTarefas)
+        itemDOMCampoNovaTarefaNome.value = ''
+        itemDOMCampoNovaTarefaNome.focus()
+    }
+});
